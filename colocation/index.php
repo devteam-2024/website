9e9d743ae80402570b1048253403c8c2ad8be15c
Lukas <lukas.rensberg@arineo-external.com>
2024-07-12 10:02:57 +0200

    Changed Files and and Colocation page

---
+++ colocation/index.php
@@ -0,0 +1,13 @@
+
+   <?php include("../header.php") ?>
+    <main class="container">
+        <h1 class="center">Colocation</h1>
+        
+        <p class="center">Wir können dir zu attraktiven Konditionen Colocation in</br>
+        unseren Stellflächen im firstColo-Rechenzentrum anbieten. </br>
+        Kontaktiere uns gerne für ein unverbindliches Angebot per </br>
+        Mail unter folgender Adresse: colocation@europeservices.de</br></p>
+    </main>
+</body>
+</html>
+
--- support/index.php
+++ support/index.php
@@ -6,7 +6,7 @@
         <div class="grid-container">
             <div class="item">Discord<br><span class="desc">Contact us a DM to account:</br>lukasthedeveloper</span></div>
             <div class="item">WhatsApp<br><span class="desc">Contact us via Number:</br><a href="https://wa.me/4915117895616" class="desc">+49 1511 7895616</a></span></div>
-            <div class="item">E-Mail<br><span class="desc">Contact us via E-Mail-Address:</br><a href="mailto://info@europeservices.de" class="desc">info@europeservices.de</a></span></div>
+            <div class="item">E-Mail<br><span class="desc">Contact us via E-Mail-Address:</br><a href="mailto:support@europeservices.de?subject=Support%20Request&body=Hallo%2C%0A%0A" class="desc">support@europeservices.de</a></span></div>
         </div>
     </main>
 </body>
