dcf2d81ac6acaeed904aac6b556faabdd295052c
Lukas Rensberg <lukas.rensberg@arineo-external.com>
2024-07-12 09:11:46 +0200

    Changing files to v1.0.1. See Releases Tab

--- header.php
+++ header.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" class="windows desktop landscape">
+<head>
+    <meta charset="UTF-8">
+    <meta meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>ES : <?= (!empty(explode("/", $_SERVER['REQUEST_URI'])[1])) ? ucfirst(explode("/", $_SERVER['REQUEST_URI'])[1]) : "Home"; ?></title>
+    <link rel="icon" type="image/png" sizes="32x32" href="/logos/favicon-32x32.png">
+    <link rel="stylesheet" href="pricing.css">
+    <link rel="stylesheet" href="/test.css">
+    <link rel="stylesheet" href="support.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/fontawesome.min.css">
+</head>
+
+<body>
 <header style="height: 64.4px;">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/fontawesome.min.css">
     <a href="/"><img src="../logos/logo_transparent.png" width="150px" id="logo" alt="Logo von Tube Hosting"></a>
 
     <div class="center">
--- support/index.php
+++ support/index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" class="windows desktop landscape">
-<head>
-    <meta charset="UTF-8">
-    <meta meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>ES : Pricing</title>
-    <link rel="icon" type="image/png" sizes="32x32" href="/logos/favicon-32x32.png">
-    <link rel="stylesheet" href="support.css">
-    <link rel="stylesheet" href="/test.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/fontawesome.min.css">
-</head>
 
-<body>
    <?php include("../header.php") ?>
     <main class="container">
         <h1 class="center">Supportmöglichkeiten</h1>
